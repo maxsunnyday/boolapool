d1dd26cd591e9af3e9d25be1aa387d0d261dd68d
maxsunnyday <max.sun@yale.edu>
2019-07-24 15:58:07 -0400

    fixed logic

--- frontend/src/functions.js
+++ frontend/src/functions.js
@@ -40,6 +40,12 @@ function displayLogin() {
             } else {
                 localStorage.setItem("user_id", data.id)
                 loginDiv.innerHTML = ""
+                document.querySelector("div#loginBtn").remove()
+                createProfileBtn()
+                createLogoutBtn()
+                document.querySelector("div#all").addEventListener("click", function (e) {
+                    displayTrips()
+                })
                 displayProfile(data)
             }
 		})
@@ -96,6 +102,12 @@ function displayLogin() {
 				.then(data => {
 					localStorage.setItem("user_id", data.id)
                     loginDiv.innerHTML = ""
+                    document.querySelector("div#loginBtn").remove()
+                    createProfileBtn()
+                    createLogoutBtn()
+                    document.querySelector("div#all").addEventListener("click", function (e) {
+                        displayTrips()
+                    })
                     displayProfile(data)
 			    })
 			})
@@ -256,4 +268,85 @@ function displayTrips() {
         }
         // console.log(tbody)
     })
+
+    listenNewTrip()
+}
+
+function createLoginBtn() {
+    const loginButton = document.createElement("div")
+    loginButton.id = "loginBtn"
+    loginButton.className = "btn btn-outline-primary"
+    loginButton.innerText = "Login/Signup"
+    toolbar.appendChild(loginButton)
+}
+
+function listenNewTrip() {
+    //Create new Trip
+    document.addEventListener('submit', function(e){
+        if (e.target.className === "new-trip") {
+            e.preventDefault()
+
+            let tbody = document.querySelector('tbody')
+
+            fetch("http://localhost:3000/trips", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify({
+                    destination: e.target.children[1].value,
+                    address: e.target.children[3].value,
+                    capacity: e.target.children[5].value,
+                    start_time: e.target.children[7].value,
+                    end_time: e.target.children[9].value,
+                    user_id: userId
+                })
+            }).then(response => response.json())
+            .then(trip => {
+                let start = new Date(trip.start_time)
+                let end = new Date(trip.end_time)
+
+                let start_formatted = start.getUTCFullYear() + "-" + appendLeadingZeroes(start.getUTCMonth() + 1) + "-" + appendLeadingZeroes(start.getUTCDate()) + " " + appendLeadingZeroes(start.getUTCHours()) + ":" + appendLeadingZeroes(start.getUTCMinutes())
+
+                let end_formatted = end.getUTCFullYear() + "-" + appendLeadingZeroes(end.getUTCMonth() + 1) + "-" + appendLeadingZeroes(end.getUTCDate()) + " " + appendLeadingZeroes(end.getUTCHours()) + ":" + appendLeadingZeroes(end.getUTCMinutes())
+
+                let tr = document.createElement("tr")
+
+                tr.innerHTML = `<td>${trip.destination}</td>
+                <td>${trip.address}</td>
+                <td>${start_formatted} - ${end_formatted}</td>
+                <td>${trip.users.length}/${trip.capacity}</td>
+                <td>Already Joined!</td>`
+
+                tbody.appendChild(tr)
+            })
+        }
+    })
+}
+
+function createLogoutBtn() {
+    const logoutButton = document.createElement("div")
+    logoutButton.id = "logout"
+    logoutButton.className = "btn btn-outline-primary"
+    logoutButton.innerText = "Logout"
+    toolbar.appendChild(logoutButton)
+    // Logout functionality
+    logoutButton.addEventListener('click', function(e){
+        localStorage.removeItem('user_id')
+        document.querySelector("div#logout").remove()
+        document.querySelector("div#profile").remove()
+        createLoginBtn()
+        displayLogin()
+    })
+}
+
+function createProfileBtn() {
+    const profileBtn = document.createElement("div")
+    profileBtn.id = "profile"
+    profileBtn.className = "btn btn-outline-primary"
+    profileBtn.innerText = "Profile"
+    toolbar.appendChild(profileBtn)
+
+    listenProfileBtn()
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,80 +9,16 @@ document.addEventListener('DOMContentLoaded', function(){
 	//Login method
 	if (localStorage.getItem('user_id')) {
 		let userId = localStorage.getItem('user_id')
-		const profileBtn = document.createElement("div")
-		profileBtn.id = "profile"
-		profileBtn.className = "btn btn-outline-primary"
-		profileBtn.innerText = "Profile"
-		toolbar.appendChild(profileBtn)
-
-		listenProfileBtn()
-
-		const logoutButton = document.createElement("div")
-		logoutButton.id = "logout"
-		logoutButton.className = "btn btn-outline-primary"
-		logoutButton.innerText = "Logout"
-		toolbar.appendChild(logoutButton)
-		// Logout functionality
-		logoutButton.addEventListener('click', function(e){
-			localStorage.removeItem('user_id')
-			displayLogin()
-		})
+		createProfileBtn()
+		createLogoutBtn()
 		
-
 		//Display Trips
 		document.querySelector("div#all").addEventListener("click", function (e) {
 			displayTrips()
 		})
 
-		//Create new Trip
-		document.addEventListener('submit', function(e){
-			if (e.target.className === "new-trip") {
-				e.preventDefault()
-
-				let tbody = document.querySelector('tbody')
-
-				fetch("http://localhost:3000/trips", {
-					method: "POST",
-					headers: {
-						'Content-Type': 'application/json',
-						'Accept': 'application/json'
-					},
-					body: JSON.stringify({
-						destination: e.target.children[1].value,
-						address: e.target.children[3].value,
-						capacity: e.target.children[5].value,
-						start_time: e.target.children[7].value,
-						end_time: e.target.children[9].value,
-						user_id: userId
-					})
-				}).then(response => response.json())
-				.then(trip => {
-					let start = new Date(trip.start_time)
-					let end = new Date(trip.end_time)
-
-					let start_formatted = start.getUTCFullYear() + "-" + appendLeadingZeroes(start.getUTCMonth() + 1) + "-" + appendLeadingZeroes(start.getUTCDate()) + " " + appendLeadingZeroes(start.getUTCHours()) + ":" + appendLeadingZeroes(start.getUTCMinutes())
-
-					let end_formatted = end.getUTCFullYear() + "-" + appendLeadingZeroes(end.getUTCMonth() + 1) + "-" + appendLeadingZeroes(end.getUTCDate()) + " " + appendLeadingZeroes(end.getUTCHours()) + ":" + appendLeadingZeroes(end.getUTCMinutes())
-
-					let tr = document.createElement("tr")
-
-					tr.innerHTML = `<td>${trip.destination}</td>
-					<td>${trip.address}</td>
-					<td>${start_formatted} - ${end_formatted}</td>
-					<td>${trip.users.length}/${trip.capacity}</td>
-					<td>Already Joined!</td>`
-
-					tbody.appendChild(tr)
-				})
-			}
-		})
-
 	} else {
-		const loginButton = document.createElement("div")
-		loginButton.id = "loginBtn"
-		loginButton.className = "btn btn-outline-primary"
-		loginButton.innerText = "Login/Signup"
-		toolbar.appendChild(loginButton)
+		createLoginBtn()
 		displayLogin()
 	}
 })
