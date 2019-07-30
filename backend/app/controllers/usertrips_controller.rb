076bc9df47abf9034db96f2dedbcee008a9ceb1f
maxsunnyday <max.sun@yale.edu>
2019-07-30 16:28:59 -0400

    unjoined text message almost done

--- backend/app/controllers/usertrips_controller.rb
+++ backend/app/controllers/usertrips_controller.rb
@@ -22,10 +22,18 @@ class UsertripsController < ApplicationController
     def destroy
         usertrip = Usertrip.find(params[:id])
         trip = usertrip.trip
-        user = usertrip.user
+        unjoined_user = usertrip.user
         usertrip.destroy
-        UserMailer.with(unjoined_user: user, trip: trip).unjoin_user_email.deliver_later
-        UserMailer.with(unjoined_user: user, trip: trip).unjoin_notification_email.deliver_later
+        
+        client = Twilio::REST::Client.new Rails.application.credentials.twilio_account_sid, Rails.application.credentials.twilio_auth_token
+
+        client.messages.create from: '+14752243879', to: "+1#{unjoined_user.phone}", body: "You just unjoined a trip to #{trip.destination}"
+
+        trip.users.each do |user|
+            client.messages.create from: '+14752243879', to: "+1#{user.phone}", body: "#{unjoined_user.full_name} just unjoined your trip to #{trip.destination}"
+        end
+        UserMailer.with(unjoined_user: unjoined_user, trip: trip).unjoin_user_email.deliver_later
+        UserMailer.with(unjoined_user: unjoined_user, trip: trip).unjoin_notification_email.deliver_later
     end
  
     private
