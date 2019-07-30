f044dac6d329a2f3aca63b24c19f2cca038a3d44
maxsunnyday <max.sun@yale.edu>
2019-07-30 09:47:47 -0400

    secrets file

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :trips
   resources :users
   post "/login", to: "users#login"
+  post 'twilio/voice' => 'twilio#voice'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
