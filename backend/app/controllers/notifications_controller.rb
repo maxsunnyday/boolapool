bddb8fcc7fc8bf7c6c3ca34bfe099dde3b8860b7
maxsunnyday <max.sun@yale.edu>
2019-08-01 00:16:44 -0400

    small notifications controller change

--- backend/app/controllers/notifications_controller.rb
+++ backend/app/controllers/notifications_controller.rb
@@ -5,7 +5,7 @@ class NotificationsController < ApplicationController
     def notify
         client = Twilio::REST::Client.new Rails.application.credentials.twilio_account_sid, Rails.application.credentials.twilio_auth_token
 
-        message = client.messages.create from: '+14752243879', to: '+16263848699', body: 'Wassup', media_url: 'http://linode.rabasa.com/yoda.gif'
+        message = client.messages.create from: '+14752243879', to: '+19193280069', body: 'Wassup', media_url: 'http://linode.rabasa.com/yoda.gif'
         # , status_callback: request.base_url + '/twilio/status'
 
         render plain: message.status
